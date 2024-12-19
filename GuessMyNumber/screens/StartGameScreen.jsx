10c09d7a2adf6df0ec2db0061dfc47637a2fcca9
Dzhem Rushen Syuleyman <122823771+superGemHere@users.noreply.github.com>
2024-12-19 19:34:00 +0200

    feat: change style of input and some restrictions

--- GuessMyNumber/screens/StartGameScreen.jsx
+++ GuessMyNumber/screens/StartGameScreen.jsx
@@ -1,12 +1,47 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { View, Text, TextInput, StyleSheet } from "react-native";
+import React from "react";
+import PrimaryButton from "../components/PrimaryButton";
 
 const StartGameScreen = () => {
   return (
-    <View>
-      <Text>StartGameScreen</Text>
+    <View style={styles.inputContainer}>
+      <TextInput
+        style={styles.input}
+        maxLength={2}
+        keyboardType={"number-pad"}
+        autoCapitalize="none"
+        autoCorrect={false}
+      />
+      <PrimaryButton>Reset</PrimaryButton>
+      <PrimaryButton>Confirm</PrimaryButton>
     </View>
-  )
-}
+  );
+};
 
-export default StartGameScreen
+export default StartGameScreen;
+
+const styles = StyleSheet.create({
+  inputContainer: {
+    padding: 16,
+    marginTop: 100,
+    marginHorizontal: 24,
+    borderRadius: 8,
+    backgroundColor: "#72063c",
+    elevation: 4,
+    shadowColor: "black",
+    shadowOffset: { width: 0, height: 2 },
+    shadowRadius: 6,
+    shadowOpacity: 0.25
+  },
+  input: {
+    height: 70,
+    width: 50,
+    fontSize: 32,
+    textAlign: "center",
+    color: "#ddb52f",
+    borderBottomColor: "#ddb52f",
+    borderBottomWidth: 2,
+    marginVertical: 8,
+    fontWeight: "bold"
+  }
+});
