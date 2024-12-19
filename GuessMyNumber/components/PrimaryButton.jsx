577d2298ad30e093532c3f1434809b71faa6ccca
Dzhem Rushen Syuleyman <122823771+superGemHere@users.noreply.github.com>
2024-12-19 20:08:58 +0200

    feat: make button pressable + styling

--- GuessMyNumber/components/PrimaryButton.jsx
+++ GuessMyNumber/components/PrimaryButton.jsx
@@ -1,14 +1,48 @@
-import { View, Text, Pressable } from 'react-native'
-import React from 'react'
+import { View, Text, Pressable, StyleSheet } from "react-native";
+import React from "react";
 
-const PrimaryButton = ({children}) => {
+const PrimaryButton = ({ children }) => {
+  const pressHandler = () => {
+    console.log(`${children} button pressed`);
+  };
+
   return (
-      // <Pressable>
-         <View>
-            <Text>{children}</Text>
-         </View>
-      // </Pressable>
-  )
-}
+    <View style={styles.buttonOuterCointainer}>
+      <Pressable
+        style={({ pressed }) =>
+          pressed
+            ? [styles.buttonInnerContainer, styles.pressed]
+            : styles.buttonInnerContainer}
+        onPress={pressHandler}
+        android_ripple={{ color: "#640233" }}
+      >
+        <Text style={styles.buttonText}>
+          {children}
+        </Text>
+      </Pressable>
+    </View>
+  );
+};
 
-export default PrimaryButton
+export default PrimaryButton;
+
+const styles = StyleSheet.create({
+  buttonOuterCointainer: {
+    borderRadius: 28,
+    margin: 4,
+    overflow: "hidden"
+  },
+  buttonInnerContainer: {
+    backgroundColor: "#72063c",
+    paddingVertical: 8,
+    paddingHorizontal: 16,
+    elevation: 2
+  },
+  buttonText: {
+    color: "#fff",
+    textAlign: "center"
+  },
+  pressed: {
+    opacity: 0.75
+  }
+});
