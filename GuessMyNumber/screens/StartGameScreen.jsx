3ebc3c436499cc3b3c329d570da2166887125ede
Dzhem Rushen Syuleyman <122823771+superGemHere@users.noreply.github.com>
2024-12-20 20:07:09 +0200

    feat: change darker background color

--- GuessMyNumber/screens/StartGameScreen.jsx
+++ GuessMyNumber/screens/StartGameScreen.jsx
@@ -34,7 +34,7 @@ const styles = StyleSheet.create({
     marginTop: 100,
     marginHorizontal: 24,
     borderRadius: 8,
-    backgroundColor: "#4e0329",
+    backgroundColor: "#3b021f",
     elevation: 4,
     shadowColor: "black",
     shadowOffset: { width: 0, height: 2 },
