977a308034026b1fa2ef65e5cc1087a4e77de6bc
Dzhem Rushen Syuleyman <122823771+superGemHere@users.noreply.github.com>
2024-12-20 21:19:23 +0200

    feat: create reusable title component

---
+++ GuessMyNumber/components/Title.jsx
@@ -0,0 +1,21 @@
+import { Text, StyleSheet } from "react-native";
+import React from "react";
+import Colors from "../constants/colors";
+
+const Title = ({ children }) => {
+  return <Text style={styles.title}>{children}</Text>;
+};
+
+export default Title;
+
+const styles = StyleSheet.create({
+  title: {
+    fontSize: 24,
+    fontWeight: "bold",
+    color: Colors.accent500,
+    textAlign: "center",
+    padding: 12,
+    borderWidth: 2,
+    borderColor: Colors.accent500,
+  }
+});
