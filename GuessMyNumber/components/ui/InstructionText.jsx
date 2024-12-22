75cecf20c1cf7701a98f1348c5050a41d216d0b5
Dzhem Rushen Syuleyman <122823771+superGemHere@users.noreply.github.com>
2024-12-22 20:16:15 +0200

    feat: prop additional styles

---
+++ GuessMyNumber/components/ui/InstructionText.jsx
@@ -0,0 +1,20 @@
+import { View, Text, StyleSheet } from "react-native";
+import React from "react";
+import Colors from "../../constants/colors";
+
+const InstructionText = ({ children, additionalStyles }) => {
+  return (
+    <Text style={[styles.instructionText, additionalStyles]}>
+      {children}
+    </Text>
+  );
+};
+
+export default InstructionText;
+
+const styles = StyleSheet.create({
+  instructionText: {
+    color: Colors.accent500,
+    fontSize: 24
+  }
+});
