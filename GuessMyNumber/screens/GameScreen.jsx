30a7cf3bd0ab1c5d53f47df5dad99eea74138ede
Dzhem Rushen Syuleyman <122823771+superGemHere@users.noreply.github.com>
2024-12-22 20:17:06 +0200

    chore: switch to re-usable components

--- GuessMyNumber/screens/GameScreen.jsx
+++ GuessMyNumber/screens/GameScreen.jsx
@@ -1,8 +1,11 @@
 import { View, Text, StyleSheet, Alert } from "react-native";
 import React, { useEffect, useState } from "react";
+import { Ionicons } from "@expo/vector-icons";
 import Title from "../components/ui/Title";
 import NumberContainer from "../components/game/NumberContainer";
 import PrimaryButton from "../components/ui/PrimaryButton";
+import Card from "../components/ui/Card";
+import InstructionText from "../components/ui/InstructionText";
 
 const generateRandomBetween = (min, max, exclude) => {
   const rndNum = Math.floor(Math.random() * (max - min)) + min;
@@ -17,12 +20,7 @@ let minBoundary = 1;
 let maxBoundary = 100;
 
 const GameScreen = ({ userNumber, onGameOver }) => {
- 
-  const initialGuess = generateRandomBetween(
-    1,
-    100,
-    userNumber
-  );
+  const initialGuess = generateRandomBetween(1, 100, userNumber);
 
   const [currentGuess, setCurrentGuess] = useState(initialGuess);
 
@@ -65,17 +63,23 @@ const GameScreen = ({ userNumber, onGameOver }) => {
       <NumberContainer>
         {currentGuess}
       </NumberContainer>
-      <View>
-        <Text>Higher or lower?</Text>
-        <View>
-          <PrimaryButton onPress={nextGuessHandler.bind(this, "lower")}>
-            -
-          </PrimaryButton>
-          <PrimaryButton onPress={nextGuessHandler.bind(this, "greater")}>
-            +
-          </PrimaryButton>
+      <Card>
+        <InstructionText additionalStyles={{ marginBottom: 5 }}>
+          Higher or lower?
+        </InstructionText>
+        <View style={styles.buttonsContainer}>
+          <View style={styles.buttonWrapper}>
+            <PrimaryButton onPress={nextGuessHandler.bind(this, "lower")}>
+              <Ionicons name="remove" size={24} color="white" />
+            </PrimaryButton>
+          </View>
+          <View style={styles.buttonWrapper}>
+            <PrimaryButton onPress={nextGuessHandler.bind(this, "greater")}>
+              <Ionicons name="add" size={24} color="white" />
+            </PrimaryButton>
+          </View>
         </View>
-      </View>
+      </Card>
       <View>
         <Text>Log Rounds</Text>
       </View>
@@ -89,5 +93,13 @@ const styles = StyleSheet.create({
   screen: {
     flex: 1,
     padding: 12
+  },
+  buttonsContainer: {
+    flex: 1,
+    flexDirection: "row",
+    marginTop: 24
+  },
+  buttonWrapper: {
+    flex: 1
   }
 });
