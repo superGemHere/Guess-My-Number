e2206e9871fdfc457a50ebfd10dc10534eb2fe49
Dzhem Rushen Syuleyman <122823771+superGemHere@users.noreply.github.com>
2024-12-22 19:45:54 +0200

    chore: fix callstack issue

--- GuessMyNumber/screens/GameScreen.jsx
+++ GuessMyNumber/screens/GameScreen.jsx
@@ -1,5 +1,5 @@
 import { View, Text, StyleSheet, Alert } from "react-native";
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import Title from "../components/ui/Title";
 import NumberContainer from "../components/game/NumberContainer";
 import PrimaryButton from "../components/ui/PrimaryButton";
@@ -16,14 +16,24 @@ const generateRandomBetween = (min, max, exclude) => {
 let minBoundary = 1;
 let maxBoundary = 100;
 
-const GameScreen = ({ userNumber }) => {
+const GameScreen = ({ userNumber, onGameOver }) => {
+ 
   const initialGuess = generateRandomBetween(
-    minBoundary,
-    maxBoundary,
+    1,
+    100,
     userNumber
   );
 
   const [currentGuess, setCurrentGuess] = useState(initialGuess);
+
+  useEffect(
+    () => {
+      if (currentGuess === userNumber) {
+        onGameOver();
+      }
+    },
+    [currentGuess, userNumber, onGameOver]
+  );
 
   const nextGuessHandler = direction => {
     if (
